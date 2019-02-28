b029ea2fef2045b5c418bb317a007dba817033f7
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-02-28 09:14:52 -0800

    fixed code for square...its really a rectangle?

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,6 +3,7 @@
 class square():
     
     width = 0
+    height = 0
 
     
     def __init__(self, *args, **kwargs):
@@ -14,14 +15,14 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
