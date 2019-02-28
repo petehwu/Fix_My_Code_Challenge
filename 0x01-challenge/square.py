107a90b331bb8cb54367f98f6c48109dd9ce7798
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-02-28 10:11:17 -0800

    added error checking to make sure height = width

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,25 +7,21 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        h = kwargs.get('height')
+        w = kwargs.get('width')
+        if (h is not None and w is not None and h == w):
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        if (self.width >= self.height):
-            return self.height * self.height
-        else:
-            return self.width * self.width
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        if (self.width >= self.height):
-            return (self.height * 4)
-        else:
-            return (self.width * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
-
 
 if __name__ == "__main__":
 
